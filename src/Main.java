4c77a36781782022f82823bbbce31540f26d3ca7
PascalNothacker <57908719+PascalNothacker@users.noreply.github.com>
2020-04-08 11:56:02 +0200

    Revert "Merge branch 'master' of https://github.com/xXAriiiXx/SoftwareQuali"
    This reverts commit d02b92d827f4cdb14a28766b3de1c44da8818a56, reversing
    changes made to 4f593231b0ecbe30d36b08c1362a70d07bb2eab2.

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,5 @@
-
 public class Main {
     public static void main(String[] args) {
-        /*
-        Input values are the same, same output is expected everywhere
-         */
-        int numberElements = 12;
-        double lambda = 0.1;
-        BaseCalculation[] allSystems = new BaseCalculation[5];
-        allSystems[0] = new Ari(numberElements, lambda);
-        allSystems[1] = new Pascal(numberElements,lambda);
-        allSystems[2] = new Maxi(numberElements, lambda);
-        allSystems[3] = new Simon(numberElements, lambda);
-
-
-        /*
-        4 inputs the same, one is different, voting should still allow
-        */
-        int oneDifferentElement = 6;
-
-        /*
-        2 Inputs different, voting should fail, go into safe state
-        */
-        int twoDifferentElement = 4;
-
-    }
-
-    public int Voting(BaseCalculation[] allSystems)
-    {
-        int numberOfSameAnswers = 1;
-        for(int i = 1; i<allSystems.length; i++)
-        {
-            if( (allSystems[0].calculateParallelMTTF()-allSystems[i].calculateParallelMTTF()) < 0.000001)
-                if((allSystems[0].calculateSerialMTTF()-allSystems[i].calculateSerialMTTF()) < 0.000001)
-                    numberOfSameAnswers++;
-        }
-        if(numberOfSameAnswers == allSystems.length)
-            return 1;
-        else if(numberOfSameAnswers == allSystems.length-1)
-            return 0;
-        else
-            return -1;
-    }
-
-    public void Ausgabe(int status)
-    {
-        if(status == 1)
-            System.out.println("Keine Fehler aufgetreten!");
-        else if(status == 0)
-            System.out.println("1 Fehler aufgetreten!");
-        else
-            System.out.println("Mehrere Fehler aufgetreten! Nehme sicheren Zustand ein!");
+        System.out.println("Hello World!");
     }
 }
