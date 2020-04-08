7ba04ecd328a7e61ad1e8bab96ed0a3c63d82b90
maximilian.rosnauer <inf17204@lehre.dhbw-stuttgart.de>
2020-04-08 11:39:11 +0200

    Added Maxi class

---
+++ src/Maxi.java
@@ -0,0 +1,56 @@
+public class Maxi extends BaseCalculation {
+
+    /**
+     * Constuctor for this class. The parameters used in the calculations
+     * must be passed.
+     * @param elements the number of elements.
+     * @param lambda the value of lambda.
+     */
+    public Maxi(int elements, double lambda) {
+        super(elements, lambda);
+    }
+
+    /**
+     * Calculates the factorial of a given number.
+     * @param n the number to calculate the factorial of.
+     * @return the factorial of the input number.
+     */
+    private int factorial(int n) {
+        int result = 1;
+        for(int i = n; i > 0; i--) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    /**
+     * Calculates the parallel Mean-Time-To-Failure for
+     * the parameters passed in the constructor of this class.
+     * The calculation is based on double precision.
+     * @return the parallel MTTF (double-precision).
+     */
+    @Override
+    double calculateParallelMTTF() {
+        double sumResult = 0;
+        for(int i = 1; i <= this.elements; i++) {
+            double expResult = Math.pow(-1, this.elements + 1);
+            double numerator = this.factorial(this.elements);
+            double denominator = (double)i * (double)this.factorial(i) * (double) this.factorial(this.elements - i);
+
+            sumResult = sumResult + expResult * (numerator / denominator);
+        }
+
+        return sumResult / this.lambda;
+    }
+
+    /**
+     * Calculates the serial Mean-Time-To-Failure for
+     * the parameters passed in the constructor of this class.
+     * The calculation is based on double precision.
+     * @return the serial MTTF (double-precision).
+     */
+    @Override
+    double calculateSerialMTTF() {
+        return 1/(this.lambda * this.elements);
+    }
+}
